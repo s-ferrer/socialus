3bd3f2d80e8e6732536cdbacd769aa9041e17685
s-ferrer <saraferrer@gmail.com>
2022-11-23 12:45:31 +0100

    Create schemas comment, pin, postedBy, save

---
+++ socialus_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'documnet',
+  fields: [
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    },
+  ],
+};
---
+++ socialus_backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'save',
+  title: 'Save',
+  type: 'documnet',
+  fields: [
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'userId',
+      title: 'UserId',
+      type: 'string',
+    },
+  ],
+};

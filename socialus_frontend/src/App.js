7e2216f4281ca10d60d7ab27ef61914645460382
s-ferrer <saraferrer@gmail.com>
2022-11-24 15:46:49 +0100

    Fixing Login with Google

--- socialus_frontend/src/App.js
+++ socialus_frontend/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import Login from './components/Login';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import Login from './components';
 import Home from './container/Home';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User =
+      localStorage.getItem('user') !== 'undefined'
+        ? JSON.parse(localStorage.getItem('user'))
+        : localStorage.clear();
+
+    if (!User) navigate('/login');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <Routes>
-      <Route path='login' element={<Login />} />
-      <Route path='/*' element={<Home />} />
-    </Routes>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
   );
 };
 
--- socialus_frontend/src/components/Login.jsx
+++ socialus_frontend/src/components/Login.jsx
@@ -1,46 +1,66 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from 'react';
+//import { GoogleLogin } from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
 import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/share.mp4';
+import shareVideo from '../assets/black.mp4';
 import logo from '../assets/logowhite.png';
+// eslint-disable-next-line no-unused-vars
+import { gapi } from 'gapi-script';
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
+import { client } from '../client';
 
 const Login = () => {
-  const responseGoogle = (response) => {};
+  const navigate = useNavigate();
+  const [profile, setProfile] = useState([]);
+
+  const onSuccess = (res) => {
+    var decoded = jwt_decode(res.credential);
+    setProfile(decoded);
+    localStorage.setItem('user', JSON.stringify(decoded));
+    if (decoded.sub > 1) {
+      const doc = {
+        _type: 'user',
+        _id: profile.sub,
+        userName: profile.given_name,
+        image: profile.picture,
+        email: profile.email,
+        firstName: profile.given_name,
+        lastName: profile.family_name,
+      };
+      console.log(doc);
+      client.createOrReplace(doc).then(() => {
+        navigate('/', { replace: true });
+      });
+    }
+  };
+
+  const onFailure = (res) => {
+    console.log('Login Failed');
+  };
 
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
-      <div className='relative w-full h-full'>
+      <div className=' relative w-full h-full'>
         <video
           src={shareVideo}
           type='video/mp4'
-          Loop
+          loop
           controls={false}
           muted
           autoPlay
           className='w-full h-full object-cover'
         />
+
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay'>
           <div className='p-5'>
-            <img src={logo} width='130px' alt='logo' />
+            <img src={logo} alt='logo' width='130px' />
           </div>
-          <div className='shadow-2x1'>
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-              render={(renderProps) => (
-                <button
-                  type='button'
-                  className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className='mr-4' /> Sign in with Google
-                </button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy='single_host_origin'
-            />
+
+          <div className='shadow-2xl'>
+            <GoogleLogin onSuccess={onSuccess} onFailure={onFailure} />;
           </div>
         </div>
       </div>
